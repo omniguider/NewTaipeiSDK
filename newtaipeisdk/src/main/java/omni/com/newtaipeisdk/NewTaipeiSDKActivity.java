0b2b611bace30b4767bc7135ab5bd927f881cae6
ok0225 <leo.hsiao@omniguider.com>
2020-11-19 10:13:44 +0800

    modify beacon layout

--- newtaipeisdk/src/main/java/omni/com/newtaipeisdk/NewTaipeiSDKActivity.java
+++ newtaipeisdk/src/main/java/omni/com/newtaipeisdk/NewTaipeiSDKActivity.java
@@ -65,7 +65,7 @@ public class NewTaipeiSDKActivity extends BaseBleActivity implements BeaconConsu
         add("7016");
     }};
     public static final List<String> NLPI_BEACON_ID_LIST = new ArrayList<String>();
-    public static final String BEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+    public static final String BEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:29-29,i:34-39,i:40-41,i:46-47";
     private static final float beaconTrigger10 = 10f;
     private static final int beaconNum = 1620;
     private static final String uid = "F0CE7A96E6A4";
